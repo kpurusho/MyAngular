08779aecfc56bd383e2b7f4df4d5dd5fb256170a
Karthik Purushothaman <karthk.purushothaman@gmail.com>
2020-06-14 22:27:46 +0530

    basic service implementation

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { IProduct } from './product';
 
 @Injectable(
-    providedIn : 'root'
 )
 export class ProductService{
     getProducts() : IProduct[] {
